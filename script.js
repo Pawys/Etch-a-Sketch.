c05edddc58a31e5463b9c5d8f248b59b076db9d3
Pawys <88272261+Pawys@users.noreply.github.com>
2023-08-28 13:50:00 +0200

    Add New Grid Button

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const container = document.querySelector('.container');
-let boxnumber = 10;
-for (let i = 0; i < boxnumber; i++){
-  const column = document.createElement('div');
-  column.classList.add('column');
+const columns = document.querySelectorAll('.column');
+function createGrid(){
+  removeAll()
+  let boxnumber = getNumber();
   for (let i = 0; i < boxnumber; i++){
-    const div = document.createElement('div');
-    div.classList.add('box');
-    div.addEventListener("mouseover", () => {
-      div.classList.add('boxhovered');
-    });
-    column.appendChild(div);
+    const column = document.createElement('div');
+    column.classList.add('column');
+    for (let i = 0; i < boxnumber; i++){
+      const div = document.createElement('div');
+      div.classList.add('box');
+      div.addEventListener("mouseover", () => {
+        div.classList.add('boxhovered');
+      });
+      column.appendChild(div);
+    }
+    container.appendChild(column);
   }
-  container.appendChild(column);
+}
+function removeAll(){
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+function getNumber() {
+
+  let number = prompt("Number of Boxes Per Side")
+  while (number === null || number > 100 || number < 1|| number % 1 !== 0) {
+   number = prompt("Number of Boxes Per Side");
+  }
+    return number;
 }
